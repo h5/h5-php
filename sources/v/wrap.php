8607da1f3088d6fe1440c27e54bfebe2c8c5d34a
unknown <user@.(none)>
2011-08-14 18:34:56 +0800

    added h5.png as favicon

--- sources/v/wrap.php
+++ sources/v/wrap.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title></title>
-  <link rel="shortcut icon" href="<?php echo URI_BASE ?>/assets/i/favicon.ico">
+  <link rel="icon" type="image/png" href="<?php echo URI_BASE ?>/assets/i/h5.png">
 
   <script>(function(B,C){B[C]=B[C].replace(/\bno-js\b/,'js');if(!/*@cc_on!@*/0)return;var e = "abbr article aside audio canvas command datalist details figure figcaption footer header hgroup mark meter nav output progress section summary time video".split(' '),i=e.length;while(i--){document.createElement(e[i])}})(document.documentElement,'className');</script>
   <link rel="stylesheet" href="<?php echo URI_BASE ?>/assets/h5.css" media="screen, projection" />
