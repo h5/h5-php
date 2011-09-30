3a2ceb2d22d22ae09470cd1e6e8c030be68dd75d
U-n61j\cuhuak <cuhuak@n61j.(none)>
2011-10-01 04:15:49 +0800

    minor editions

--- sources/v/html/version.php
+++ sources/v/html/version.php
@@ -2,4 +2,4 @@
 // please do not delete this file and leave link to the official website. thanks. :]
 $_decorator = false;
 ?>
-h5 framework, version 0.1, github.com/cuhuak/h5
+h5 framework, version 0.1, github.com/h5
