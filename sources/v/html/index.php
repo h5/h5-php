bd432db02fab55327d62c30484cfd4c698dd7dc7
U-PC409-2\USER <USER@PC409-2.(none)>
2011-11-02 14:26:59 +0800

    clean&dry, edited welcome page

--- sources/v/html/index.php
+++ sources/v/html/index.php
@@ -1,1 +1,6 @@
-<h1>Highfive!</h1>
+<div class="w18 q15 e15 prepend-top">
+  <h5><img src="<?php echo URI_BASE ?>/assets/i/h5.png" alt="h5 framework"> highfive!</h5>
+
+  <p>To start edit this file at sources/v/html/index.php</p>
+  <p class="quiet">Happy coding...</p>
+</div>
