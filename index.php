ba06e30a59fea2ad3299823b195d5ed07e253d1b
Sinitsyn Evgeny <cuhuak@gmail.com>
2012-05-19 04:19:40 +0800

    added route to hello world

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ require_once 'sources/h5.php';
 
 $routes = array(
 
-  '^/h5/form$'  => array('h5/css/form', 'get'),
+  '^/h5/form$'      => array('h5/css/form', 'get'),
   '^/h5\.css'       => array('h5/css/generate', 'get'),
+
+   '^/hello/(?<world>[\w-]+)$'       => array('hello', 'get'),
 
   '^/(?<page>[\w-]+).html$' => array('html', 'get'),
   '^/$'                     => array('html', 'get'),
