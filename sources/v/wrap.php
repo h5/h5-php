fdb42666e18638217fdf5bc902c879072f9fcf94
Sinitsyn Evgeny <cuhuak@gmail.com>
2012-05-31 02:12:14 +0800

    added css file to wrap

--- sources/v/wrap.php
+++ sources/v/wrap.php
@@ -7,6 +7,7 @@
 
   <script>(function(B,C){B[C]=B[C].replace(/\bno-js\b/,'js');if(!/*@cc_on!@*/0)return;var e = "abbr article aside audio canvas command datalist details figure figcaption footer header hgroup mark meter nav output progress section summary time video".split(' '),i=e.length;while(i--){document.createElement(e[i])}})(document.documentElement,'className');</script>
   <link rel="stylesheet" href="<?php echo URI_BASE ?>/h5.css" media="screen, projection" />
+  <link rel="stylesheet" href="<?php echo URI_BASE ?>/assets/style.css" media="screen, projection" />
 </head>
 
 <body>
